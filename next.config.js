cdba02b83f6e7477b5fb57421e926e5bc9b949d9
alron <alron.jan.lam@gmail.com>
2023-04-26 23:33:24 +0800

    Add gh-pages

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const repo = 'personal-portfolio'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+}
